467068231ed2b997bffafc30aa812ffc3041d2f4
Gabriel Christino Cordeiro dos Santos <gabriel.christino@gmail.comm>
2022-01-27 15:23:10 -0300

    product photo

--- publish.bat
+++ publish.bat
@@ -1,10 +1,5 @@
-cd C:\Users\gabri\workspace\meuestoque
-call npm run pwa
-cd C:\Users\gabri\workspace\gabrielchristino.github.io
-del * /Q
-rmdir /S assets /Q
-xcopy /S C:\Users\gabri\workspace\meuestoque\dist\estoque C:\Users\gabri\workspace\gabrielchristino.github.io
+npm run pwa
 git add .
-git commit -a -m "update"
+git commit -a
 git push
-pause
+firebase deploy
